8e2d8d061c60d16b705c245861daca40525f5e24
yotamputra <yotamppy@gmail.com>
2025-02-12 13:12:37 +0700

    feat: create user test

--- test/user.test.ts
+++ test/user.test.ts
@@ -3,17 +3,15 @@ import { app } from "../src/application/app";
 import { logger } from "../src/application/logging";
 
 describe("POST /api/users", () => {
-  it('should reject register new user if request is invalid', async () => {
-    const response = await supertest(app)
-        .post("/api/users")
-        .send({
-            username: "",
-            password: "",
-            name: ""
-        });
+  it("should reject register new user if request is invalid", async () => {
+    const response = await supertest(app).post("/api/users").send({
+      username: "",
+      password: "",
+      name: "",
+    });
 
     logger.debug(response.body);
     expect(response.status).toBe(400);
     expect(response.body.errors).toBeDefined();
-});
+  });
 });
