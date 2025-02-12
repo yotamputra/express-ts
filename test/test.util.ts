0a5aae7ee61c4689aea42c721a7fafed3c275ca5
yotamputra <yotamppy@gmail.com>
2025-02-12 14:37:32 +0700

    feat: create testing for login user

--- test/test.util.ts
+++ test/test.util.ts
@@ -1,10 +1,22 @@
 import { prismaClient } from "../src/application/database";
+import { hashPass } from "../src/helpers/bcrypt";
 
 export class UserTest {
   static async delete() {
     await prismaClient.user.deleteMany({
       where: {
         username: "test",
+      },
+    });
+  }
+
+  static async create() {
+    await prismaClient.user.create({
+      data: {
+        username: "test",
+        name: "test",
+        password: hashPass("test"),
+        token: "test",
       },
     });
   }
--- test/user.test.ts
+++ test/user.test.ts
@@ -33,3 +33,39 @@ describe("POST /api/users", () => {
     expect(response.body.data.name).toBe("test");
   });
 });
+
+describe("POST /api/users/login", () => {
+  beforeEach(async () => {
+    await UserTest.create();
+  });
+
+  afterEach(async () => {
+    await UserTest.delete();
+  });
+
+  it("should reject login if request is invalid", async () => {
+    const response = await supertest(app).post("/api/users/login").send({
+      username: "",
+      password: "",
+    });
+
+    logger.debug(response.body);
+    expect(response.status).toBe(400);
+    expect(response.body.errors).toBeDefined();
+  });
+
+  it("should return token", async () => {
+    const response = await supertest(app).post("/api/users/login").send({
+      username: "test",
+      password: "test",
+    });
+
+    console.log(response.body, "<----");
+
+    logger.debug(response.body);
+    expect(response.status).toBe(200);
+    expect(response.body.data.username).toBe("test");
+    expect(response.body.data.name).toBe("test");
+    expect(response.body.data.token).toBeDefined();
+  });
+});
