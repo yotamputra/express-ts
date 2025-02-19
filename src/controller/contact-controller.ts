991595265aa541c85f52e7c587e114bebfe34de5
yotamputra <yotamppy@gmail.com>
2025-02-19 11:18:00 +0700

    feat: create api for contacts

---
+++ src/controller/contact-controller.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Response } from "express";
+import { UserRequest } from "../type/user-request";
+import { CreateContactRequest } from "../model/contact-model";
+import { ContactService } from "../service/contact-service";
+
+export class ContactController {
+  static async create(req: UserRequest, res: Response, next: NextFunction) {
+    try {
+      const request: CreateContactRequest = req.body as CreateContactRequest;
+      const response = ContactService.create(req.user!, request);
+
+      res.status(201).json({
+        data: response,
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+}
--- src/route/api.ts
+++ src/route/api.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import { auth } from "../middleware/auth";
 import { UserController } from "../controller/user-controller";
+import { ContactController } from "../controller/contact-controller";
 
 export const apiRouter = express.Router();
 
 apiRouter.use(auth);
 
+// User
 apiRouter.get("/api/users/current", UserController.getUser);
 apiRouter.patch("/api/users/current", UserController.update);
-apiRouter.delete("/api/users/current", UserController.logout)
+apiRouter.delete("/api/users/current", UserController.logout);
+
+//Contact
+apiRouter.post("/api/contacts", ContactController.create);
