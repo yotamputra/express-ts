da03d9d69ff575759c0595efb8b271d20fec2b94
yotamputra <yotamppy@gmail.com>
2025-02-21 11:23:30 +0700

    test: create test for create address if contact not found

--- test/address.test.ts
+++ test/address.test.ts
@@ -57,4 +57,23 @@ describe("POST /api/contacts/:contactId/addresses", () => {
     expect(res.status).toBe(400);
     expect(res.body.errors).toBeDefined();
   });
+
+  it("should reject create address if request is contact is not found", async () => {
+    const contact = await ContactTest.get();
+
+    const res = await supertest(app)
+      .post(`/api/contacts/${contact.id + 1}/addresses`)
+      .set("X-API-TOKEN", "test")
+      .send({
+        street: "test",
+        city: "test",
+        province: "test",
+        country: "test",
+        postal_code: "12345",
+      });
+
+    logger.debug(res.body);
+    expect(res.status).toBe(404);
+    expect(res.body.errors).toBeDefined();
+  });
 });
