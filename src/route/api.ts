57cdc53edd062508a2cbe8a84f8b0a7e6a35430c
yotamputra <yotamppy@gmail.com>
2025-02-21 11:11:50 +0700

    feat: create api for create address

--- src/route/api.ts
+++ src/route/api.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { auth } from "../middleware/auth";
 import { UserController } from "../controller/user-controller";
 import { ContactController } from "../controller/contact-controller";
+import { AddressController } from "../controller/address-controller";
 
 export const apiRouter = express.Router();
 
@@ -12,9 +13,15 @@ apiRouter.get("/api/users/current", UserController.getUser);
 apiRouter.patch("/api/users/current", UserController.update);
 apiRouter.delete("/api/users/current", UserController.logout);
 
-//Contact
+// Contact
 apiRouter.post("/api/contacts", ContactController.create);
 apiRouter.get("/api/contacts/:contactId(\\d+)", ContactController.get); // Regex for number
 apiRouter.put("/api/contacts/:contactId(\\d+)", ContactController.update);
 apiRouter.delete("/api/contacts/:contactId(\\d+)", ContactController.remove);
 apiRouter.get("/api/contacts", ContactController.search);
+
+// Address
+apiRouter.post(
+  "/api/contacts/:contactId(\\d+)/addresses",
+  AddressController.create
+);
---
+++ src/service/address-service.ts
@@ -0,0 +1,19 @@
+import { User } from "@prisma/client";
+import { AddressResponse, CreateAddressRequest, toAddressResponse } from "../model/address-model";
+import { Validation } from "../validation/validation";
+import { AddressValidation } from "../validation/address-validation";
+import { ContactService } from "./contact-service";
+
+export class AddressService {
+  static async create(user: User, request: CreateAddressRequest): Promise<AddressResponse> {
+    const createRequest = Validation.validate(AddressValidation.CREATE, request);
+
+    await ContactService.checkContactExists(user.username, request.contact_id)
+
+    const address = await prismaClient.address.create({
+      data: createRequest
+    })
+
+    return toAddressResponse(address);
+  }
+}
